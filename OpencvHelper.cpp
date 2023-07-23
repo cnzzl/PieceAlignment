c0080b899e38f9189444b45783d4274ee20eaecb
cnzzl <105800010+cnzzl@users.noreply.github.com>
2023-07-24 00:36:45 +0800

    Update OpencvHelper.cpp

--- OpencvHelper.cpp
+++ OpencvHelper.cpp
@@ -1,1 +1,21 @@
-
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d.hpp>
+using namespace cv;
+using namespace cv::xfeatures2d;
+using namespace std;
+class OpenCVClass {
+public:
+    // 读取图像
+    Mat readImage(string filename) {
+        Mat image = imread(filename, IMREAD_COLOR);
+        return image;
+    }
+    // 显示图像
+    void showImage(Mat image, string windowName) {
+        namedWindow(windowName, WINDOW_AUTOSIZE);
+        imshow(windowName, image);
+    }
+    // 保存图像
+    void saveImage(Mat image, string filename) {
+        imwrite(filename, image);
+    }
